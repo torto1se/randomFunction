f72892a1f99802ab91567566ca0beefecd094f2b
torto1se <akvvsjcvdhi@gmail.com>
2023-12-05 17:54:59 +0300

    update

--- random.js
+++ random.js
@@ -1,4 +1,4 @@
-const imageUrls = {
+let imageUrls = {
     'https://biggirlslimming.files.wordpress.com/2015/10/5.jpg': 5,
     'https://st3.depositphotos.com/1016929/14228/i/1600/depositphotos_142282828-stock-photo-3d-gold-80-eighty-percent.jpg': 80,
     'https://thumbs.dreamstime.com/b/gold-fifty-percent-discount-sign-sale-off-sale-banner-template-special-offer-off-tag-minus-sticker-sale-94221871.jpg': 50,
@@ -12,18 +12,18 @@ function getRandomNumber(max) {
 }
 
 function generateRandomImage() {
-  const randomUrls = Object.keys(imageUrls);
-  const randomIndex = getRandomNumber(randomUrls.length);
-  const randomImageUrl = randomUrls[randomIndex];
-  const randomImageElement = document.getElementById('randomImage');
+  let randomUrls = Object.keys(imageUrls);
+  let randomIndex = getRandomNumber(randomUrls.length);
+  let randomImageUrl = randomUrls[randomIndex];
+  let randomImageElement = document.getElementById('randomImage');
   randomImageElement.src = randomImageUrl;
-  const discountText = document.getElementById('discountText');
-  const discountPercentage = imageUrls[randomImageUrl];
+  let discountText = document.getElementById('discountText');
+  let discountPercentage = imageUrls[randomImageUrl];
   discountText.innerText = `Вы получили скидку в размере ${discountPercentage}%`;
 }
 
 function toggleDiscount() {
-  const button = document.querySelector('button');
+  let button = document.querySelector('button');
   if (button.innerText === 'Получить скидку') {
     generateRandomImage();
     button.innerText = 'Вернуться на главную страницу';
@@ -33,9 +33,9 @@ function toggleDiscount() {
 }
 
 function resetPage() {
-  const button = document.querySelector('button');
-  const randomImageElement = document.getElementById('randomImage');
-  const discountText = document.getElementById('discountText');
+  let button = document.querySelector('button');
+  let randomImageElement = document.getElementById('randomImage');
+  let discountText = document.getElementById('discountText');
   button.innerText = 'Получить скидку';
   randomImageElement.src = 'https://novostidnja.ru/wp-content/uploads/2023/08/67738207_s-e1525278398508.jpg';
   discountText.innerText = '';
